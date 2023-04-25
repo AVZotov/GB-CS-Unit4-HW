6e8621561e27fbc6c7d04012bf6bb72fc0d3ad93
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-26 01:04:22 +0300

    Exersise003 project added

---
+++ Exersise003/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] FillArray(int[] userArray)
+{
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        Console.Write($"Enter {i + 1} value to add into the array: ");
+        userArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return userArray;
+}
+
+Console.Clear();
+int[] userArray = new int[8];
+FillArray(userArray);
+Console.WriteLine(String.Join(", ", userArray));
