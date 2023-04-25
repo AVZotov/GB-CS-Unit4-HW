c3f23c6fb5abeecd685aa4e7a3e0bd8bdf39eb02
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-26 00:17:22 +0300

    Exersise001 project added

---
+++ Exersise001/Program.cs
@@ -0,0 +1,18 @@
+﻿int GetDegree(int value, int degree)
+{
+    int result = 1;
+
+    for (int i = 1; i <= degree; i++)
+    {
+        result *= value;
+    }
+
+    return result;
+}
+
+Console.Clear();
+Console.Write("Enter a value: ");
+int value = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter degree: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{value}^{degree} degree = {GetDegree(value, degree)}");
