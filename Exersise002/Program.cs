b7ce1219d3dbed9c1743666324795be016ce7c34
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-26 00:52:35 +0300

    Exersise002 project added

---
+++ Exersise002/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetCount(int value)
+{
+    int result = 0;
+
+    while (value > 0)
+    {
+        result += value % 10;
+        value /= 10;
+    }
+
+    return result;
+}
+
+int number = 452;
+Console.WriteLine($"Summ of numbers in value = {GetCount(number)}");
